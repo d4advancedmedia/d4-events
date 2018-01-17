0e940fd235b830e207e95b41bfba4a0d164bcba0
GrayofD4 <GrayofD4@users.noreply.github.com>
2018-01-17 14:58:43 -0800

    fix issue is extension isn't processed.

--- lib/user_func.php
+++ lib/user_func.php
@@ -216,7 +216,9 @@ function d4events_get_files($files) {
 			} else {
 				$file_name = end((explode('/', rtrim($meta[1], '/'))));
 			}
-			$file_class = 'fileclass_'.pathinfo($meta[1])['extension'];
+			if ( isset($meta[1]['extension']) ) {
+				$file_class = 'fileclass_'.$meta[1]['extension'];
+			}
 			$file_link = '<a href="'.$meta[1].'" class="events_files '.$file_class.'" target="_blank">'.$file_name.'</a>';
 			
 			$file_array[] = array(
