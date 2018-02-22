be615d960bd6cd9286cad0beaf5ff5a27486bf43
grayson <grayson@d4am.com>
2018-02-22 13:39:34 -0800

    set to dev

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 	Plugin Name: D4 Events
 	Plugin URI: https://www.d4am.com/d4-events/
 	Description: Simple events manager plugin
-	Version: 4.0.0
+	Version: 4.0.0-dev
 	Author: D4 Advanced Media
 */
 
-$d4events_version = '4.0.0';
+$d4events_version = '4.0.0-dev';
 
 include ('config.php');
 include ('modules/index.php');
