1dd2b46d51b7c914d6f57f4963f72192069374b1
Unknown <grayson@d4webdesign.com>
2018-05-17 12:12:38 -0700

    move d4events_fetch_datetime()

--- backend/admin_func.php
+++ backend/admin_func.php
@@ -30,26 +30,3 @@ function d4events_timezone_list($postid) {
 	return $output;
 }
 
-//Fetch and unmerge separate dates and times from a single string composed of a single unix timestamp  (start-end)
-function d4events_fetch_datetime($postid) {
-
-	$start = get_post_meta($postid,'d4events_start',true);
-	$end = get_post_meta($postid,'d4events_end',true);
-
-	if ( ($start == '') || ($end == '') ) {
-		#$meta = date("m/d/Y");
-		$meta = 'not available';
-	} 
-
-	else {
-
-		$meta_array['d4events_start_date'] = date('m/d/Y',$start);
-		$meta_array['d4events_end_date'] = date('m/d/Y',$end);
-
-		$meta_array['d4events_start_time'] = date('g:ia',$start);
-		$meta_array['d4events_end_time'] = date('g:ia',$end);
-
-	}
-
-	return $meta_array;
-}
